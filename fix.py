54266cb1b7ec492782dfffa548802303799b90da
steinshark <everettperson@gmail.com>
2024-04-01 16:18:02 -0400

    Fix experience naming

---
+++ fix.py
@@ -0,0 +1,26 @@
+import os 
+
+experiences     = {}
+
+PATH            = "C:/data/chess/mcts_train/"
+
+for filename in os.listdir(PATH):
+    id      = filename[:5]
+    
+    print(f"id is {id}")
+
+    with open(os.path.join(PATH,filename),'r') as file1obj:
+        if id in experiences:
+            experiences[id].append(file1obj.read())
+        else:
+            experiences[id] = [file1obj.read()]
+
+for id in experiences:
+
+    for i,contents in enumerate(experiences[id]):
+
+        #Create new file
+        fname           = f"C:/data/chess/game_experiences/{id}_{i}"
+        with open(fname,'w') as fileobj:
+            fileobj.write(contents)
+        fileobj.close()
